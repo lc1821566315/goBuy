86abc2dc3dcaa90d516c6084f7c61b6f2097571a
zeyu <1821566315@qq.com>
2018-07-25 19:51:06 +0800

    项目基本结构结构搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+// 引入路由模块
+// import VueRouter from 'vue-router'
 import App from './App.vue'
+// 路由中间件
+// Vue.use(VueRouter)
 
+
+// 引入css文件
+import './assets/statics/site/css/style.css';
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el:'#app',
+  render: h => h(App),
+})
